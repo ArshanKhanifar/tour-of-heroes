019ad1b364704e9d314634cec30adb07bf724937
arshan <arshankhanifar@gmail.com>
2017-08-16 16:11:38 -0400

    made this commit to push the project to github

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
+import { OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  constructor(
+    private heroService: HeroService,
+    private router: Router
+    
+  ) { }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
